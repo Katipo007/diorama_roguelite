ac2343debed89d9a7724f117f9f38cd911998244
Ben <ben@steadfast.co.nz>
2021-03-13 20:36:51 +1300

    Dedicated server hello world

---
+++ DedicatedServer/DedicatedServerMain.cpp
@@ -0,0 +1,12 @@
+#include "Precompiled/pch.hpp"
+
+#include "Common/Core/Base.hpp"
+
+#include <iostream>
+
+int main( int argc, char** argv )
+{
+	std::cout << "Hello world! - Dedicated Server" << std::endl;
+
+	return 0;
+}
