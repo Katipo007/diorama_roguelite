4f929eb0efb49aa5d15b0f7c4ec41cbcc4d5e2bd
Ben <ben@steadfast.co.nz>
2021-03-13 20:36:36 +1300

    Client hello world

---
+++ Client/Precompiled/pch.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef DIORAMA_ROGUELITE_PCH_HPP
+#define DIORAMA_ROGUELITE_PCH_HPP
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "Common/Core/Base.hpp"
+#include "Common/Core/Logging.hpp"
+
+#endif // DIORAMA_ROGUELITE_PCH_HPP
