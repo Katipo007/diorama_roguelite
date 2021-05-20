b2d9c8a110a573a69c98b89c605d646da98b631e
Ben <ben@steadfast.co.nz>
2021-05-21 00:22:57 +1200

    ASSERT macro fixes

--- Common/Core/Assert.hpp
+++ Common/Core/Assert.hpp
@@ -7,7 +7,7 @@
 
 #if defined(ENABLE_ASSERTS)
 
-#	define INTERNAL_ASSERT_IMPL(channel, check, msg, ...) { if(!(check)) { SPDLOG_LOGGER_ERROR( ::Logging::GetLogger( Logging::Channel::channel ), msg, __VA_ARGS__ ); DEBUGBREAK(); } }
+#	define INTERNAL_ASSERT_IMPL(channel, check, msg, ...) do { if(!(check)) { SPDLOG_LOGGER_ERROR( ::Logging::GetLogger( Logging::Channel::channel ), msg, __VA_ARGS__ ); DEBUGBREAK(); } } while(0)
 #	define INTERNAL_ASSERT_WITH_MSG(channel, check, ...) INTERNAL_ASSERT_IMPL(channel, check, "Assertion failed: {2} at {0}:{1}", std::filesystem::path(__FILE__).filename().string(), __LINE__, __VA_ARGS__)
 #	define INTERNAL_ASSERT_NO_MSG(channel, check) INTERNAL_ASSERT_IMPL(channel, check, "Assertion '{0}' failed at {1}:{2}", STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
@@ -18,12 +18,12 @@
 #	define ASSERT(...) EXPAND_MACRO( INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(Assert, __VA_ARGS__) )
 #	define ASSERT_CHANNEL(channel, ...) EXPAND_MACRO( INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(channel, __VA_ARGS__) )
 
-#	define NOT_IMPLEMENTED { LOG_ERROR( Assert, "Function not implemented!" ); DEBUGBREAK(); }
+#	define NOT_IMPLEMENTED do { LOG_ERROR( Assert, "Function not implemented!" ); DEBUGBREAK(); } while(0)
 #else
-#	define ASSERT(x, ...) (void)0
-#	define ASSERT_CHANNEL(channel, x, ...) (void)0
+#	define ASSERT(x, ...) do {} while(0)
+#	define ASSERT_CHANNEL(channel, x, ...) do {} while(0)
 
-#	define NOT_IMPLEMENTED { LOG_ERROR( Assert, "Function not implemented!" ); }
+#	define NOT_IMPLEMENTED do { LOG_ERROR( Assert, "Function not implemented!" ); } while(0)
 #endif
 
 #define FATAL(msg) do { LOG_CRITICAL( Assert, "Application has encountered a fatal error and will abort. {0}", msg ); DEBUGBREAK(); abort(); } while(0)
