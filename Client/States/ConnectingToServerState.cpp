531074daca49f55b6dd2f7c22209bccf23bdf216
Ben <ben@steadfast.co.nz>
2021-05-28 20:18:04 +1200

    ConnectingToServerState now displays a status message and handles transitioning to the InGameState

--- Client/States/ConnectingToServerState.cpp
+++ Client/States/ConnectingToServerState.cpp
@@ -14,11 +14,27 @@ namespace ClientStates
 		return fsm::Actions::NoAction();
 	}
 
+	void ConnectingToServerState::OnLeave()
+	{
+		if (connection != nullptr)
+			DetatchFromConnection( *connection );
+	}
+
 	fsm::Actions::Might<fsm::Actions::TransitionTo<JoinMultiplayerState>> ConnectingToServerState::HandleEvent( const DearImGuiFrameEvent& )
 	{
 		if (ImGui::Begin( "ConnectingToServerState", NULL, ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove ))
 		{
-			ImGui::Text( "Connecting to server..." );
+			std::string_view status_message = "Connecting to server...";
+
+			if (connection)
+			{
+				if (connection->IsConnected())
+					status_message = "Waiting for login...";
+				else if (connection->IsDisconnected())
+					status_message = "Disconnected.";
+			}
+
+			ImGui::Text( status_message.data() );
 
 			if (ImGui::Button( "Cancel" ))
 			{
@@ -33,24 +49,50 @@ namespace ClientStates
 		return fsm::Actions::NoAction{};
 	}
 
-	fsm::Actions::Might<fsm::Actions::TransitionTo<InGameState>> ConnectingToServerState::HandleEvent( const ServerMessageEvent& )
+	fsm::Actions::Might<fsm::Actions::TransitionTo<InGameState>> ConnectingToServerState::HandleEvent( const ServerMessageEvent& e )
 	{
+		using namespace Networking::ClientServer;
+
+		switch (e.message.GetType())
+		{
+			case MessageFactory::GetMessageType<Messages::ServerClientLoginSuccess>() :
+			{
+				const auto& result = static_cast<const Messages::ServerClientLoginSuccess&>(e.message);
+				LOG_INFO( Client, "Login success message received, our username is '{}'", result.username.data() );
+				e.FlagAsHandled();
+				return fsm::Actions::TransitionTo<ClientStates::InGameState>();
+				break;
+			}
+
+			default:
+				break;
+		}
+
 		return fsm::Actions::NoAction{};
 	}
 
 	fsm::Actions::NoAction ConnectingToServerState::HandleEvent( const ConnectedToServerEvent& e )
 	{
-		using namespace Networking::ClientServer;
-		e.connection.SendMessage<Messages::ClientServerChatMessage>( ChannelType::Reliable, []( Messages::ClientServerChatMessage& msg )
-			{
-				StringUtility::StringToArray( "Test", msg.message );
-			} );
-
+		AttachToConnection( e.connection );
 		return fsm::Actions::NoAction();
 	}
 
-	fsm::Actions::TransitionTo<DisconnectedFromServerState> ConnectingToServerState::HandleEvent( const DisconnectedFromServerEvent& )
+	fsm::Actions::TransitionTo<DisconnectedFromServerState> ConnectingToServerState::HandleEvent( const DisconnectedFromServerEvent& e )
 	{
+		DetatchFromConnection( e.connection );
 		return fsm::Actions::TransitionTo<DisconnectedFromServerState>();
+	}
+
+	void ConnectingToServerState::AttachToConnection( Networking::ClientServer::ServerConnection& connection_ )
+	{
+		assert( connection == nullptr );
+		connection = &connection_;
+	}
+
+	void ConnectingToServerState::DetatchFromConnection( Networking::ClientServer::ServerConnection& connection_ )
+	{
+		assert( connection != nullptr );
+		assert( &connection_ == connection );
+		connection = nullptr;
 	}
 }
--- Client/States/ConnectingToServerState.hpp
+++ Client/States/ConnectingToServerState.hpp
@@ -6,6 +6,8 @@
 #include "Common/Utility/StateMachine/Actions/TransitionTo.hpp"
 #include "Common/Utility/NonCopyable.hpp"
 
+namespace Networking::ClientServer { class ServerConnection; }
+
 namespace ClientStates
 {
 	class DisconnectedFromServerState;
@@ -26,6 +28,7 @@ namespace ClientStates
 		~ConnectingToServerState() = default;
 
 		fsm::Actions::NoAction OnEnter( const ConnectingToServerEvent& );
+		void OnLeave();
 
 		fsm::Actions::Might<fsm::Actions::TransitionTo<JoinMultiplayerState>> HandleEvent( const DearImGuiFrameEvent& e );
 		fsm::Actions::Might<fsm::Actions::TransitionTo<InGameState>> HandleEvent( const ServerMessageEvent& e );
@@ -35,5 +38,11 @@ namespace ClientStates
 	private:
 		ConnectingToServerState( const ConnectingToServerState& ) = delete;
 		ConnectingToServerState operator=( const ConnectingToServerState& ) = delete;
+
+		void AttachToConnection( Networking::ClientServer::ServerConnection& connection );
+		void DetatchFromConnection( Networking::ClientServer::ServerConnection& connection );
+
+	private:
+		Networking::ClientServer::ServerConnection* connection = nullptr;
 	};
 }
