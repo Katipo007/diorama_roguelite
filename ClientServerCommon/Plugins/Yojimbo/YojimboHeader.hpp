95abc9ff746f6c138797227b971a7a7e1eb968d4
Ben <ben@steadfast.co.nz>
2021-05-30 22:22:29 +1200

    Move yojimbo library from Common/ to ClientServerCommon/

--- ClientServerCommon/Plugins/Yojimbo/YojimboHeader.hpp
+++ ClientServerCommon/Plugins/Yojimbo/YojimboHeader.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #pragma warning(push, 0)
-#include "Common/Vendor/yojimbo/yojimbo.h"
+#include "ClientServerCommon/Vendor/yojimbo/yojimbo.h"
 #pragma warning(pop)
 
 #ifdef _WIN64
 #	ifdef _DEBUG
-#		pragma comment(lib, "Common/Vendor/yojimbo/bin/x64/Debug/yojimbo.lib")
+#		pragma comment(lib, "ClientServerCommon/Vendor/yojimbo/bin/x64/Debug/yojimbo.lib")
 #	else
-#		pragma comment(lib, "Common/Vendor/yojimbo/bin/x64/Release/yojimbo.lib")
+#		pragma comment(lib, "ClientServerCommon/Vendor/yojimbo/bin/x64/Release/yojimbo.lib")
 #	endif
 #else
 #error Haven't setup lib paths for yojimbo.lib on x86 (if yojimbo even supports x86)
