4a053ce8f840e27d6cba7e9d27439879d007bb72
Ben <ben@steadfast.co.nz>
2021-06-01 22:46:44 +1200

    Refactor fsm::TransitionTo SFINAE using concepts

--- Common/Utility/StateMachine/TransitionTo.hpp
+++ Common/Utility/StateMachine/TransitionTo.hpp
@@ -9,63 +9,28 @@ namespace fsm
 {
 	namespace detail
 	{
-		// Primary template with a static assertion
-		// for a meaningful error message
-		// if it ever gets instantiated.
-		// We could leave it undefined if we didn't care.
-
-		template<typename, typename T>
-		struct has_OnEnter {
-			static_assert(
-				std::integral_constant<T, false>::value,
-				"Second template parameter needs to be of function type.");
+		template<typename T>
+		concept HasDefaultOnEnterMethod = requires(T t)
+		{
+			{ t.OnEnter() } -> Concepts::Action;
 		};
 
-		template<typename, typename T>
-		struct has_OnLeave {
-			static_assert(
-				std::integral_constant<T, false>::value,
-				"Second template parameter needs to be of function type.");
+		template<typename T, typename... Args>
+		concept HasOnEnterMethod = requires( T t )
+		{
+			{ t.OnEnter( std::declval<Args>()... ) } -> Concepts::Action;
 		};
 
-		// specialization that does the checking
-
-		template<typename C, typename... Args>
-		struct has_OnEnter<C, Args... > {
-		private:
-			template<typename T>
-			static constexpr auto check( T* )
-				-> typename
-				std::is_object<
-						decltype(std::declval<T>().OnEnter( std::declval<Args>()... ))
-				>::type;  // attempt to call it and see if the return type is correct
-
-			template<typename>
-			static constexpr std::false_type check( ... );
-
-			typedef decltype(check<C>( 0 )) type;
-
-		public:
-			static constexpr bool value = type::value;
+		template<typename T>
+		concept HasDefaultOnLeaveMethod = requires(T t)
+		{
+			{ t.OnLeave() } -> std::same_as<void>;
 		};
 
-		template<typename C, typename... Args>
-		struct has_OnLeave<C, Args... > {
-		private:
-			template<typename T>
-			static constexpr auto check( T* )
-				-> typename
-				std::is_object<
-				decltype(std::declval<T>().OnLeave( std::declval<Args>()... ))
-				>::type;  // attempt to call it and see if the return type is correct
-
-			template<typename>
-			static constexpr std::false_type check( ... );
-
-			typedef decltype(check<C>( 0 )) type;
-
-		public:
-			static constexpr bool value = type::value;
+		template<typename T, typename... Args>
+		concept HasOnLeaveMethod = requires( T t )
+		{
+			{ t.OnLeave( std::declval<Args>()... ) } -> std::same_as<void>;
 		};
 	}
 
@@ -95,20 +60,20 @@ namespace fsm
 		// NOTE: using SFINAE to detect whether a OnEnter or OnLeave method was defined for a given transition type
 
 		void LeaveState( ... ) {}
-		auto EnterState( ... ) -> NoAction { return NoAction{}; }
+		Concepts::Action auto EnterState( ... ) { return NoAction{}; }
 
 		// version which doesn't take event
-		template<Concepts::State NewState, Concepts::Event Event, typename = typename std::enable_if<!detail::has_OnEnter<TargetState, const Event&>::value>::type>
-		auto EnterState( NewState& new_state, const Event& ) -> decltype(new_state.OnEnter()) { return new_state.OnEnter(); }
+		template<Concepts::State NewState, Concepts::Event Event>
+		Concepts::Action auto EnterState( NewState& new_state, const Event& ) requires(!detail::HasOnEnterMethod<NewState, const Event&> && detail::HasDefaultOnEnterMethod<NewState>) { return new_state.OnEnter(); }
 
-		template<Concepts::State PreviousState, Concepts::Event Event, typename = typename std::enable_if<!detail::has_OnLeave<TargetState, const Event&>::value>::type>
-		auto LeaveState( PreviousState& previous_state, const Event& ) -> decltype(previous_state.OnLeave()) { return previous_state.OnLeave(); }
+		template<Concepts::State PreviousState, Concepts::Event Event>
+		void LeaveState( PreviousState& previous_state, const Event& ) requires(!detail::HasOnLeaveMethod<PreviousState, const Event&> && detail::HasDefaultOnLeaveMethod<PreviousState>) { return previous_state.OnLeave(); }
 
 		// version which does take event
-		template<Concepts::State NewState, Concepts::Event Event, typename = typename std::enable_if<detail::has_OnEnter<TargetState, const Event&>::value>::type>
-		auto EnterState( NewState& new_state, const Event& event ) -> decltype(new_state.OnEnter( event )) { return new_state.OnEnter( event ); }
+		template<Concepts::State NewState, Concepts::Event Event>
+		Concepts::Action auto EnterState( NewState& new_state, const Event& event ) requires(detail::HasOnEnterMethod<NewState, const Event&>) { return new_state.OnEnter( event ); }
 
-		template<Concepts::State PreviousState, Concepts::Event Event, typename = typename std::enable_if<detail::has_OnLeave<TargetState, const Event&>::value>::type>
-		auto LeaveState( PreviousState& previous_state, const Event& event ) -> decltype(previous_state.OnLeave( event )) { return previous_state.OnLeave( event ); }
+		template<Concepts::State PreviousState, Concepts::Event Event>
+		void LeaveState( PreviousState& previous_state, const Event& event ) requires(detail::HasOnLeaveMethod<PreviousState, const Event&>) { return previous_state.OnLeave( event ); }
 	};
 }
