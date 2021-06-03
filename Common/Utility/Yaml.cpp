c0956be1ebf38acb3e3d2c165e7a2b3d3c19dafa
Ben <ben@steadfast.co.nz>
2021-06-03 21:28:57 +1200

    yaml-cpp library

---
+++ Common/Utility/Yaml.cpp
@@ -0,0 +1,7 @@
+#include "Yaml.hpp"
+
+#ifdef _DEBUG
+#	pragma comment(lib, "Common/Vendor/yaml_cpp/build/Debug/yaml-cppd.lib")
+#else
+#	pragma comment(lib, "Common/Vendor/yaml_cpp/build/Release/yaml-cpp.lib")
+#endif
---
+++ Common/Utility/Yaml.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#pragma warning(push, 0)
+#include "Common/Vendor/yaml_cpp/include/yaml-cpp/yaml.h"
+#pragma warning(pop)
