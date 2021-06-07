c32fe22bee69a5ee55fe7b003395f03d0c2f0636
Ben <ben@steadfast.co.nz>
2021-06-07 21:27:02 +1200

    Better PositionComponent

--- ClientServerCommon/Game/Transform/Components/PositionComponent.hpp
+++ ClientServerCommon/Game/Transform/Components/PositionComponent.hpp
@@ -7,11 +7,13 @@ namespace Game::Transform
 {
 	struct PositionComponent
 	{
-		std::array<float,3> position;
+		glm::vec3 position;
 
 		void Serialise( typename auto& serialiser )
 		{
-			serialiser.container4b( position );
+			serialiser.value4b( position.x );
+			serialiser.value4b( position.y );
+			serialiser.value4b( position.z );
 		}
 	};
 }
--- ClientServerCommon/Game/Transform/TransformHelpers.cpp
+++ ClientServerCommon/Game/Transform/TransformHelpers.cpp
@@ -6,17 +6,12 @@ namespace Game::Transform::Helpers
 {
 	void SetPosition( ecs::EntityHandle entity, glm::vec3 position )
 	{
-		entity.emplace_or_replace<PositionComponent>( PositionComponent{ .position = { position.x, position.y, position.z } } );
+		entity.emplace_or_replace<PositionComponent>( position );
 	}
 
 	void Move( ecs::EntityHandle entity, glm::vec3 delta )
 	{
-		entity.patch<PositionComponent>( [&]( PositionComponent& pos )
-			{
-				pos.position[0] += delta.x;
-				pos.position[1] += delta.y;
-				pos.position[2] += delta.z;
-			} );
+		entity.patch<PositionComponent>( [&]( PositionComponent& pos ) { pos.position += delta; } );
 	}
 
 	glm::vec3 GetAbsolutePosition( const ecs::EntityConstHandle entity )
@@ -26,6 +21,6 @@ namespace Game::Transform::Helpers
 
 	glm::vec3 GetPosition( const PositionComponent& position )
 	{
-		return glm::vec3{ position.position[0], position.position[1], position.position[2] };
+		return position.position;
 	}
 }
