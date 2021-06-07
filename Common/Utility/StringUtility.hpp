e0cc6da7b33016cdc5f830b0c25e77292204ed5c
Ben <ben@steadfast.co.nz>
2021-06-07 15:26:04 +1200

    Make StringToArray better

--- Common/Utility/StringUtility.hpp
+++ Common/Utility/StringUtility.hpp
@@ -1,30 +1,19 @@
 #pragma once
 
 #include <array>
-#include <algorithm>
-#include <string>
 #include <string_view>
 
 namespace StringUtility
 {
-	template<typename _T, size_t ARRAY_SIZE>
-	size_t StringToArray( std::string_view str_, std::array<_T, ARRAY_SIZE>& array_ )
+	template<size_t ARRAY_SIZE>
+	size_t StringToArray( std::string_view str_, std::array<char, ARRAY_SIZE>& array_ )
 	{
 		static_assert(ARRAY_SIZE > 0);
+		const auto src_len = strlen( str_.data() );
+		ASSERT( src_len < ARRAY_SIZE );
 
-		const size_t str_size = std::distance( std::begin( str_ ), std::end( str_ ) );
-		const size_t n_to_copy = std::min( str_size, ARRAY_SIZE );
-		if (n_to_copy <= 0)
-		{
-			array_.fill( static_cast<_T>(0) );
-			return 0;
-		}
-		else
-		{
-			std::copy_n( std::begin( str_ ), n_to_copy, std::begin( array_ ) );
-			std::fill( std::begin( array_ ) + n_to_copy, std::end( array_ ), static_cast<_T>(0) );
-			array_.back() = 0;
-			return n_to_copy;
-		}
+		memcpy_s( array_.data(), ARRAY_SIZE, str_.data(), src_len );
+		array_.back() = 0;
+		return std::min( ARRAY_SIZE, src_len );
 	}
 }
