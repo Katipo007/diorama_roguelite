931029a14df759de262c45957b4159101d4bba20
Ben <ben@steadfast.co.nz>
2021-06-11 21:00:35 +1200

    Tweak test movement system to make multiple clients visible

--- Server/Game/Movement/TestMovementSystem.cpp
+++ Server/Game/Movement/TestMovementSystem.cpp
@@ -8,6 +8,9 @@ namespace Game::Movement
 	void TestSystem( ecs::Registry& registry, const PreciseTimestep& ts )
 	{
 		for (auto entity : registry.view<Transform::PositionComponent>())
-			Transform::Helpers::SetPosition( { registry, entity }, glm::vec3{ std::sin( ts.time * 10 ) * 10, std::cos( ts.time * 10 ) * 10, 0.f } );
+		{
+			const float t = (float)ts.time * 10.f + int( entity ) * 2.f;
+			Transform::Helpers::SetPosition( { registry, entity }, glm::vec3{ std::sin( t ) * 10, std::cos( t ) * 10, 0.f } );
+		}
 	}
 }
